d084eb6d3307aea8e9466ca7c2f2f469dcd6b403
Alex Belloni <alexbelloni@outlook.com>
2017-02-07 22:11:39 -0500

    search

--- js/client.js
+++ js/client.js
@@ -8,6 +8,10 @@ function initialize(){
     show(level);
   });
 
+$('#btnGo').click( function () {
+  show('advanced');
+});
+
   //Initialize with easy level.
   show('easy');
 }
@@ -35,7 +39,7 @@ function show(level){
       loadGymhEyc2(level, showBootstrapData);
     }
     else{
-      loadAlternativeFuelLocations(level, showBootstrapData);
+      loadAlternativeFuelLocations(level, showBootstrapData, showSearchOptions);
     }
 }
 
@@ -53,12 +57,22 @@ function loadGymhEyc2(level, showData){
   });
 }
 
-function loadAlternativeFuelLocations(level, showData){
+function loadAlternativeFuelLocations(level, showData, callback){
+
+  var column = $('#textColumn').val();
+  var text = $('#textSearch').val();
+
+  var search = 'fuel_type_code=LPG';
+
+  if(column && text){
+    search = column + "=" + text;
+  }
+
   $.ajax({
-    url: "https://data.cityofchicago.org/resource/alternative-fuel-locations.json?fuel_type_code=LPG",
+    url: "https://data.cityofchicago.org/resource/alternative-fuel-locations.json?" + search,
     data: {},
     success: function(data) {
-      showData(level, data);
+      showData(level, data, callback);
     },
     error: function(msg) {
       //todo: handle error gracefully
@@ -77,32 +91,59 @@ function showDefaultData(level, data){
       presentPage(level);
 }
 
-function showBootstrapData(level, data){
+function showBootstrapData(level, data, callback){
 
       var tblName = "tbl"+level;
 
       $("#"+ level + " .panel").append('<table id="'+ tblName +'" class="table table-striped table-bordered"></table>');   
       $("#" + tblName).append("<thead><tr></tr></thead>");
 
+      if((data)&&(data.length > 0)){
+
+        //Writes all columns
+        var columns = Object.keys(data[0]);
+        columns.forEach(function(col){
+          $("#" + tblName + " thead tr").append("<td>"+ col +"</td>");
+        });
+
+        //Writes all items
+        data.forEach(function(item, i){
+
+          var rowName = tblName + "tblrow"+ i;
+
+          $("#" + tblName).append("<tr id='"+ rowName +"'></tr>");
+
+          columns.forEach(function(col){
+            $("#" + rowName).append("<td>"+ item[col] + '</td>');
+          }, item);
+
+        }, columns);
+
+        if(callback){
+          callback(data);
+        }
+      }
+
+      presentPage(level);
+}
+
+function showSearchOptions(data){
+
+      $("#dropdownSearch").empty();
+
       //Writes all columns
       var columns = Object.keys(data[0]);
       columns.forEach(function(col){
-        $("#" + tblName + " thead tr").append("<td>"+ col +"</td>");
+        $("#dropdownSearch").append('<li><span class="option" href="'+col+'">'+ col +'</span></li>');
       });
 
-      //Writes all items
-      data.forEach(function(item, i){
+    $('.option').click(function( eventObject ) {
+      var elem = $( this );
+      var text = elem.attr( "href" );
+      console.log(text);
+      $('#textColumn').val(text);
+    });
 
-        var rowName = tblName + "tblrow"+ i;
 
-        $("#" + tblName).append("<tr id='"+ rowName +"'></tr>");
-
-        columns.forEach(function(col){
-          $("#" + rowName).append("<td>"+ item[col] + '</td>');
-        }, item);
-
-      }, columns);
-
-      presentPage(level);
 }
 
