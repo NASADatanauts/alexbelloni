6dbfc3effe604586b4082aa030b194800fad2c47
alexbelloni@outlook.com <alexbelloni@outlook.com>
2019-06-16 09:20:42 -0400

    api catalog soon

---
+++ nasaApis.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    document.getElementById('apilist').innerHTML =
+        `<ul>${
+        catalog.map(api => {
+            return `<li><a href="${api.link}" target="_blank">${api.name}</a></li>`
+        }).join('')}
+        </ul>`
+}
+
+catalog = [
+    {
+        "name": "API NASA",
+        "link": "https://api.nasa.gov/"
+    },
+    {
+        "name": "TechPort",
+        "link": "http://techport.nasa.gov/home"
+    },
+    {
+        "name": "ProgrammableWeb NASA collection",
+        "link": "https://www.programmableweb.com/category/all/apis?keyword=nasa"
+    },
+    {
+        "name": "RapidApi NASA collection",
+        "link": "https://rapidapi.com/dimas/api/NasaAPI"
+    }
+]
