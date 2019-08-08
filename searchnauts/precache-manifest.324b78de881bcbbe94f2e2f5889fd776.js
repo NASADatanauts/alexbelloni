d21ee064ea96ed85158ea4d131f4f8bd42370954
Alexandre Belloni <alexbelloni@outlook.com>
2019-08-08 17:44:46 -0400

    New version of Searchnauts

---
+++ searchnauts/precache-manifest.324b78de881bcbbe94f2e2f5889fd776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acaa64b014bba902720971d91008290b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b7fecef07a50a8f4adc",
+    "url": "/static/css/2.d987b9d4.chunk.css"
+  },
+  {
+    "revision": "1664598da99b43ebc51b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0b7fecef07a50a8f4adc",
+    "url": "/static/js/2.c9597af0.chunk.js"
+  },
+  {
+    "revision": "1664598da99b43ebc51b",
+    "url": "/static/js/main.13663c75.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
